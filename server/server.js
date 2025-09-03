91eb594e94396d705e135360c4be5494f7e36479
mdayan96386 <ayan96386@gmail.com>
2025-09-04 00:59:06 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express =  require('express')
 const colors = require('colors')
-const { config } = require('dotenv')
 const connectDB = require('./config/dbconfig')
 const errorhandler = require('./midldleware/errorhandler')
 require ('dotenv').config()
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 5000;
+app.use(cors())
 
 // Connect Data Base
 connectDB()
@@ -17,7 +18,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     res.json({
-        msg: 'WELCOME TO AUTH API 1.0'
+        msg: 'WELCOME TO E-COMMERCE API 1.0'
     })
 })
 
